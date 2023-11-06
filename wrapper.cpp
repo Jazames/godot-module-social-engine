ba4f48fe108804c6303f3bc53a60db21232f7035
James <jammiesprideful@gmail.com>
2023-11-05 21:29:34 -0700

    Initial Commit

---
+++ wrapper.cpp
@@ -0,0 +1,18 @@
+
+#include "wrapper.h"
+
+String SocialEngine::generate_npc_response(String dialogue)
+{
+    std::string words = std::string(dialogue.ascii().get_data());
+    std::string reply = get_default_response(words);
+    String response = String(reply.c_str());
+    return response;
+}
+
+
+//*
+void SocialEngine::_bind_methods()
+{
+    ClassDB::bind_method(D_METHOD("generate_npc_response", "dialogue"), &SocialEngine::generate_npc_response);
+} 
+//*/
---
+++ wrapper.h
@@ -0,0 +1,30 @@
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+
+#include "core/object/ref_counted.h"
+#include "social.h"
+
+
+class SocialEngine : public RefCounted 
+{
+    GDCLASS(SocialEngine, RefCounted);
+
+protected:
+    static void _bind_methods();
+    /*
+    {
+        ClassDB::bind_method(D_METHOD("generate_npc_response", "dialogue", "appearance", "personality", "knowledge"), &SocialEngine::generate_npc_response);
+    }
+    */
+public:
+    String generate_npc_response(String dialog);
+
+    SocialEngine() = default;
+};
+
+
+
+
+
+#endif // WRAPPER_H
